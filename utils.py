76797a8c961e1aadc3e53aaeab300f7c19ae21f2
yc3332 <yc3332@columbia.edu>
2018-04-28 08:42:20 -0400

    yc3332
    the generator

--- utils.py
+++ utils.py
@@ -4,8 +4,6 @@ import numpy as np
 
 def res_block(input, filters, use_dropout=False):
 
-	padded_input = tf.pad( input, [ [0, 0], [1, 1], [1, 1], [0, 0] ], 'REFLECTION' )
-
     _out = tf.layers.conv2d(padded_input, filters=filters, kernel_size=(3, 3), strides=(1, 1), padding = 'VALID')
     _out = tf.layers.batch_normalization(_out)
     _out = tf.nn.relu(_out)
