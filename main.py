b9eb0b52ba47a521ac39f59530cae524d5579aa3
yl3832 <yl3832@columbia.edu>
2018-04-29 00:04:28 -0400

    Update
    Data loaded

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from model_tf import deblur_model
 import argparse
+from utlis import load_images
 
 if __name__ == '__main__':
     
@@ -15,7 +16,9 @@ if __name__ == '__main__':
     param = parser.parse_args()
     
     # load data
-    # ...
+    train_data = load_images('./images/train',n_images=-1)
+    test_data = load_images('./images/test', n_images=200)
+    
     
     print('Building model')
     model = deblur_model(param)
