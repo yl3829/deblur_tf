2f99ee23e40667c2f938e5a5aa0bb583d43b9455
Yanjun Lin <yl3829@columbia.edu>
2018-04-29 12:21:00 -0400

    Update

--- utils.py
+++ utils.py
@@ -15,11 +15,11 @@ def res_block(input, filters, use_dropout=False):
         _out = tf.nn.dropout( _out, keep_prob = 0.5 )
     
     _out = tf.pad( _out, [ [0, 0], [1, 1], [1, 1], [0, 0] ], mode="REFLECT" )
-    _out = tf.layers.conv2d(input, filters=filters, kernel_size=(3, 3), strides=(1, 1), padding = 'VALID')
+    _out = tf.layers.conv2d(_out, filters=filters, kernel_size=(3, 3), strides=(1, 1), padding = 'VALID')
     _out = tf.layers.batch_normalization(_out)
     
     result = tf.add( _out, input )
-    
+    result = result/2
     return result
 
 
